6a8654717f529ba8d365367a5da6eb241a2de69c
TharushikaSewwandi <tharushikasew028@gmail.com>
2025-12-17 14:57:52 +0000

    Developed Authentication & Security
    Implemented login using university database. Created login page.

---
+++ clubmanagement/src/main/java/com/example/clubmanagement/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.clubmanagement.repository;
+
+import com.example.clubmanagement.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
